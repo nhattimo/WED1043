e8ad3e87d317b56d15933bc8df0c033c47254671
Nhật Timo Code Dạo <nhatnvpd07532@fpt.edu.vn>
2023-03-09 17:04:05 +0700

    Code cùng Nhật Timo

--- WED1043/lab1/app.js
+++ WED1043/lab1/app.js
@@ -1,4 +1,7 @@
-var a = prompt("Mời bạn nhập số nguyên a",0);
-var b = prompt("mời bạn nhập số nguyên b",0);
-
-console.log(a+b);
+let a = prompt("Mời bạn nhập số nguyên a");
+a=parseInt(a,10);
+let b = prompt("mời bạn nhập số nguyên b");
+b=parseInt(b,10);
+var c=a+b;
+document.write("Tổng = " + c);
+console.log(c);
