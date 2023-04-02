15f04f34881f2235ae4e820d7d7dbd2a3e759bb8
Nhật Timo Code Dạo <nhatnvpd07532@fpt.edu.vn>
2023-04-02 23:52:20 +0700

    code

--- WED1043/lab5/app.js
+++ WED1043/lab5/app.js
@@ -1,141 +1,147 @@
-var getInput = document.getElementsByTagName('input')
-var getTextarea = document.getElementsByTagName('textarea')
-var getitem = document.querySelectorAll('.item')
-var myArray = [];
-addDanhSach();
-var dem = 0;
+var getInput = document.getElementById('ten_task');
+var getTextarea = document.getElementById('mo_ta');
+var getItemList = document.getElementById('item_list') // lất tất cả các element có class item_list
+var getH6=document.getElementsByTagName('h6')
+
 function add(){
-    var getDanhSachSTT = document.querySelectorAll('.STT')
-    var getDanhSachtask = document.querySelectorAll('.task')
-    var getDanhSachmo_ta = document.querySelectorAll('.mo_ta')
-    
-    var task = getInput[1].value;       // lấy value do người dùng nhập ở phần TASK add
-    var moTa = getTextarea[1].value;    // lấy value do người dùng nhập ở phần mô tả add
-    if(task == "" || moTa == ""){
-        alert('bạn không được để trống ô nào');
-        return;
+    var valueTask = getInput.value;             // lấy value do người dùng nhập ở phần TASK add
+    var ValueMoTa = getTextarea.value;          // lấy value do người dùng nhập ở phần mô tả add
+    if(valueTask == "" ){     // điều kiện ô nhập task và ô nhập mô tả không được bỏ trống
+        getH6[5].setAttribute('class','baoloi');
+        getH6[4].removeAttribute('class');
     }
-    // điều kiện nhập phải Task phải lớn hơn 5 ký tự
-    if(task.length < 5){
-        alert("Yêu cầu nhập task lớn hơn 5 ký tự")
-        return;
-    }
-    // điều kiện nhập mô ta phải lớn hơn 20 ký tự
-    if(moTa.length < 20){
-        alert("Yêu cầu nhập mô tả lớn hơn 20 ký tự")
-        return;
+    if(valueTask.length < 6){        
+        getH6[5].removeAttribute('class')
+        getH6[4].setAttribute('class','baoloi')
     }else{
-        addDanhSach();
-        for (let i = dem ; i <=dem; i++) {
-            dem ++;
-            console.log(i)
-            getDanhSachSTT[i].innerText = `# ${i+1}`;
-            console.log(getDanhSachSTT[i].innerText)
-            getDanhSachtask[i].innerText = getInput[1].value;
-            getDanhSachmo_ta[i].innerText = getTextarea[1].value;
-            break;
-        }
-    }   
-}
-function deleteDanhSach(deletee){
-    var con = document.getElementsByClassName('item')[4];
-    var chill = con.getElementsByClassName('danh_sach');
-    con.removeChild(chill[length])
-}
-
-
-  function editPopud(editp){
-    console.log('dd',editp)
-    var task_edit = getInput[0].value;       // lấy value do người dùng nhập ở phần TASK edit
-    var moTa1_edit = getTextarea[0].value;    // lấy value do người dùng nhập ở phần mô tả edit
-    // điều kiện không được bỏ tróng
-    if(task_edit == "" || moTa1_edit == ""){
-        alert('bạn không được để trống');
-        return;
+        getH6[4].removeAttribute('class')
+        getH6[5].removeAttribute('class')
     }
-    // điều kiện nhập phải Task phải lớn hơn 5 ký tự
-    if(task_edit.length < 5){
-        alert("Yêu cầu nhập task lớn hơn 5 ký tự")
-        return;
+    if(ValueMoTa == ""){     // điều kiện ô nhập task và ô nhập mô tả không được bỏ trống
+        getH6[7].setAttribute('class','baoloi')
+        getH6[6].removeAttribute('class')
     }
-    // điều kiện nhập mô ta phải lớn hơn 20 ký tự
-    if(moTa1_edit.length < 20){
-        alert("Yêu cầu nhập mô tả lớn hơn 20 ký tự")
-    }else{
-        for (let i = dem ; i <=dem; i++) {
-            dem ++;
-            console.log(i)
-            getDanhSachSTT[i].innerText = `# ${i+1}`;
-            console.log(getDanhSachSTT[i].innerText)
-            getDanhSachtask[i].innerText = getInput[0].value;
-            getDanhSachmo_ta[i].innerText = getTextarea[0].value;
-            break;
-        }
+    if(ValueMoTa.length < 20){
+        getH6[7].removeAttribute('class')
+        getH6[6].setAttribute('class','baoloi')
+    }else{  
+        getH6[7].removeAttribute('class')
+        getH6[6].removeAttribute('class')
+        // tạo thẻ div có class ="danh_sach"
+        let danh_sach = document.createElement('div');
+        danh_sach.setAttribute('class','danh_sach');  
+        
+        // tạo thẻ div có class ="item_trai"
+        let classItem_trai = document.createElement('div');
+        classItem_trai.setAttribute('class','item_trai');   
+
+        // tạo thẻ p có 
+        //class ="STT"
+        class_STT = document.createElement('p');
+        class_STT.className = 'STT';  
+        class_STT.innerText =`#0${document.getElementsByClassName('danh_sach').length+1}`;               
+        //class ="task"
+        var class_Task = document.createElement('p');       
+        class_Task.className = 'task';                      
+        class_Task.innerText = valueTask;             
+        //class ="mo_ta"
+        var class_Mo_ta = document.createElement('p');      
+        class_Mo_ta.className = 'mo_ta';
+        class_Mo_ta.innerText = ValueMoTa;
+
+        // tạo thẻ div có  class="item_phai"
+        let classItem_phai = document.createElement('div');
+        classItem_phai.setAttribute('class','item_phai' );
+
+        // tạo thẻ button 
+        // DELETE
+        let btnDelete = document.createElement('button');
+        btnDelete.innerText='DELETE';
+        // EDIT
+        let btnEdit = document.createElement('button');
+        btnEdit.setAttribute('onclick','myFunction()');
+        btnEdit.innerText='EDIT';
+
+        // add các thể đã tạo vào thẻ div có class item ở cuối cùng 
+        getItemList.appendChild(danh_sach);
+        danh_sach.append(classItem_trai);
+        classItem_trai.append(class_STT);
+        classItem_trai.append(class_Task);
+        classItem_trai.append(class_Mo_ta);
+        danh_sach.append(classItem_phai);
+        classItem_phai.append(btnDelete);
+        classItem_phai.append(btnEdit); 
+        
+        btnDelete.addEventListener("click", () => {
+            getItemList.removeChild(danh_sach);
+            // for (let i = 0; i < document.getElementsByClassName('danh_sach').length; i++) {
+            //     let a = document.getElementsByClassName('STT')
+            //     a[i].setAttribute('innerText','#'+[i])
+            // }
+
+        });
+        reset();
+
+        popupTask = document.getElementById('popup_ten_task')
+        popupMoTa = document.getElementById('popup_mo_ta')
+        popudUpdeta = document.getElementById('popud_Updeta')
+        popudUpdeta.addEventListener('click',() =>{
+            if(popupTask.value == "" ){     // điều kiện ô nhập task và ô nhập mô tả không được bỏ trống
+                getH6[0].removeAttribute('class')
+                getH6[1].setAttribute('class','baoloi')
+            }else if(popupTask.value.length < 6){ 
+                getH6[1].setAttribute('class','_)')          
+                getH6[0].setAttribute('class','baoloi')
+                
+            }else{
+                getH6[1].removeAttribute('class')
+                getH6[0].removeAttribute('class')
+            }
+            if(popupMoTa.value == ""){     // điều kiện ô nhập task và ô nhập mô tả không được bỏ trống
+                getH6[3].setAttribute('class','baoloi')
+                getH6[2].removeAttribute('class')
+            }else if(popupMoTa.value.length < 20){
+                getH6[3].removeAttribute('class')
+                getH6[2].setAttribute('class','baoloi')
+            }else{
+                getH6[2].removeAttribute('class')
+                getH6[3].removeAttribute('class')
+                class_Task.innerText = popupTask.value
+                class_Mo_ta.innerText = popupMoTa.value
+                resetPopup();
+            }
+        });
     }
 }
 function reset(){
-    document.getElementsByTagName('input')[1].value ="";
-    document.getElementsByTagName('textarea')[1].value ="";
+    getH6[4].removeAttribute('class')
+    getH6[5].removeAttribute('class')
+    getH6[6].removeAttribute('class')
+    getH6[7].removeAttribute('class')
+    getInput.value ="";
+    getTextarea.value ="";
 }
 function resetPopup(){
-    document.getElementsByTagName('input')[0].value ="";
-    document.getElementsByTagName('textarea')[0].value ="";
+    getH6[0].removeAttribute('class')
+    getH6[1].removeAttribute('class')
+    getH6[2].removeAttribute('class')
+    getH6[3].removeAttribute('class')
+    popupTask.value ="";
+    popupMoTa.value ="";
 }
 function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
+    var getboxpopud=document.getElementsByTagName('div')
+    getboxpopud[0].classList.add("box")
+}
+function myFunction1() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+    var getboxpopud=document.getElementsByTagName('div')
+    getboxpopud[0].setAttribute('class','box_popud')
 }
 
-function addDanhSach(){
-    console.log('___________________________________________________________');
-    const layItem = document.querySelectorAll('.item') ;// truy xuất đến class item và dùng item cuối cùng
-
-    // tạo thẻ div class danh_sach
-    const danh_sach = document.createElement('div');
-    danh_sach.setAttribute('class','danh_sach');
-
-    // tạo thẻ div class item_trai
-    const classItem_trai = document.createElement('div');
-    classItem_trai.setAttribute('class','item_trai');
-
-    // tạo thẻ p class STT
-    var class_STT = document.createElement('p');
-    class_STT.className = 'STT';
-    class_STT.innerText='...';
-    // tạo thẻ p class task
-    var class_Task = document.createElement('p');
-    class_Task.className = 'task';
-    class_Task.innerText='...............';
-    // tạo thẻ p class mo_ta
-    var class_Mo_ta = document.createElement('p');
-    class_Mo_ta.className = 'mo_ta';
-    class_Mo_ta.innerText='...........................................................................................';
-
-    // tạo thẻ div class item_trai
-    const classItem_phai = document.createElement('div');
-    classItem_phai.setAttribute('class','item_phai' );
-    // tạo thẻ buton 
-    const btnDelete = document.createElement('button');
-    btnDelete.setAttribute('onclick','deleteDanhSach(this)');
-    btnDelete.innerText='DELETE';
-    // tạo thẻ buton 
-    const btnEdit = document.createElement('button');
-    btnEdit.setAttribute('onclick','myFunction()');
-    btnEdit.innerText='EDIT';
-
-
-
-    layItem[4].appendChild(danh_sach); // add danh sách vào boxx class item
-    // console.log("lấy item4",layItem[4])
-    danh_sach.append(classItem_trai);
-    classItem_trai.append(class_STT);
-    classItem_trai.append(class_Task);
-    classItem_trai.append(class_Mo_ta);
-    danh_sach.append(classItem_phai);
-    classItem_phai.append(btnDelete);
-    classItem_phai.append(btnEdit);
-    console.log('Kết quả của 1 danh box danh sách đc tạo\n',danh_sach);
-}
 
 
 
---
+++ WED1043/lab6/app.js
@@ -0,0 +1,30 @@
+let id = 1;       // khởi tạo biến id = 1, mặc định vào trang web sẽ đứng ở ảnh 1
+showSlides(id);   // mặc định vào trang web thì sẽ nằm ảnh 1
+
+function plusSlides(value) {    // click vào sẽ lấy value
+  showSlides(id += value);      // gán id = cộng value, mỗi lần nhấn sẽ chuyển đến ảnh tiếp theo
+}
+
+function currentSlide(value) {  // click vào sẽ lấy value
+  showSlides(id = value);       // nhấn vào chấm tròn bao nhiêu sẽ chạy tới ảnh tượng tự số của ảnh
+}
+
+function showSlides(value) {
+  let i;
+  let slides = document.getElementsByClassName("slide");      
+  let chamTron = document.getElementsByClassName("cham_tron");
+  if (value > slides.length) {  // nếu số truyền vào lớn hơn lenght có nghĩa là vượt quá dộ dài của slide
+    id = 1;                     // thì gán id lại bằng 1
+  }
+  if (value < 1) {              // nếu số truyền vào nhỏ hơn 1 
+    id = slides.lengthl;        // thì slide ảnh sẽ trở về slide ảnh cuối
+  }
+  for (i = 0; i < slides.length; i++) { 
+    slides[i].style.display = "none";   // thêm style cho thẻ div có class tên là slide, với thuộc tính none
+  }
+  for (i = 0; i < chamTron.length; i++) {
+    chamTron[i].className = chamTron[i].className.replace(" active", ""); // thêm class và thẻ span, với class tên là: active
+  } 
+  slides[slideIndex-1].style.display = "block";
+  chamTron[slideIndex-1].className += " active";
+}
