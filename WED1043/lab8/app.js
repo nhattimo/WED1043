656292acc2e2ac80309cfe8ac1eb5dbcf3da0dbf
Nhật Timo Code Dạo <nhatnvpd07532@fpt.edu.vn>
2023-04-07 14:52:55 +0700

    code

--- WED1043/lab8/app.js
+++ WED1043/lab8/app.js
@@ -2,9 +2,7 @@ var vid = document.getElementById('video')
 var btn = document.getElementsByTagName('button')
 console.log(btn[5])
 function playVideo(){
-    let a=vid.play();
-    console.log(a)
-    
+    vid.play();
 }
 function pausedVideo(){
     vid.pause();
@@ -17,13 +15,11 @@ function volumeup(){
     if(vid.volume < 1){
         vid.volume = parseFloat(vid.volume + 0.1).toFixed(1)
     }
-    console.log( vid.volume = parseFloat(vid.volume + 0.1).toFixed(1))
 }
 function volumedown(){
     if(vid.volume > 0){
         vid.volume = parseFloat(vid.volume - 0.1).toFixed(1)
     }
-    console.log( vid.volume = parseFloat(vid.volume - 0.1).toFixed(1))
 }
 function loopvideo(){
     if(vid.loop){
