6f66618f6d895fae8f4457fc51055110328d9d77
Nhật Timo Code Dạo <nhatnvpd07532@fpt.edu.vn>
2023-04-07 14:51:20 +0700

    lab8

---
+++ WED1043/lab8/app.js
@@ -0,0 +1,36 @@
+var vid = document.getElementById('video')
+var btn = document.getElementsByTagName('button')
+console.log(btn[5])
+function playVideo(){
+    let a=vid.play();
+    console.log(a)
+    
+}
+function pausedVideo(){
+    vid.pause();
+}
+function stopVideo(){
+    vid.pause();
+    if(vid.currentTime){vid.currentTime = 0}
+}
+function volumeup(){
+    if(vid.volume < 1){
+        vid.volume = parseFloat(vid.volume + 0.1).toFixed(1)
+    }
+    console.log( vid.volume = parseFloat(vid.volume + 0.1).toFixed(1))
+}
+function volumedown(){
+    if(vid.volume > 0){
+        vid.volume = parseFloat(vid.volume - 0.1).toFixed(1)
+    }
+    console.log( vid.volume = parseFloat(vid.volume - 0.1).toFixed(1))
+}
+function loopvideo(){
+    if(vid.loop){
+        vid.loop = false;
+        btn[5].innerText = 'On loop play'
+    }else{
+        vid.loop = true;
+        btn[5].innerText = 'Off loop play'
+    }
+}
