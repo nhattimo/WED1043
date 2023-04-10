360638145ed0f52d13aa49e50cd5ed8f4a0bc92e
Nhật Timo Code Dạo <nhatnvpd07532@fpt.edu.vn>
2023-04-10 10:07:27 +0700

    code

--- WED1043/Assignment/app.js
+++ WED1043/Assignment/app.js
@@ -215,8 +215,6 @@ function stop(){
 }
 
 
-
-
 const text = document.querySelector('.text');
 const charArr = text.textContent.split('');
 
@@ -265,6 +263,45 @@ var getDiv =document.getElementsByTagName("div")
 console.log(getDiv)
 function off(){
   getDiv[0].setAttribute("class",'')
+}
+
+
+
+
+
+
+
+
+var x = document.getElementById("location");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred."
+      break;
+  }
 }
 
 
